7a8410fdaa9d59f23a463e4cde6ec870f82c99da
Jose Zepeda <jose-zromero@hotmail.com>
2023-08-18 19:09:00 -0600

    add creating dates for quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,9 +2,10 @@
 
 class Quote < ApplicationRecord
    # All the previous code
+  has_many :line_item_dates, dependent: :destroy
   belongs_to :company
   scope :ordered, -> { order(created_at: :asc) }  # Example ordering by created_at attribute
-
+  
    # after_create_commit -> { broadcast_prepend_later_to "quotes" }
    # after_update_commit -> { broadcast_replace_later_to "quotes" }
    # after_destroy_commit -> { broadcast_remove_to "quotes" }
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :quotes
   # Defines the root path route ("/")
+
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
