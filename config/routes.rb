97370911ed14835fdc5539533a428cf48cf1aa2d
Jose Zepeda <jose-zromero@hotmail.com>
2023-08-25 20:26:46 -0600

    Add line_item

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :quotes
   # Defines the root path route ("/")
-
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
