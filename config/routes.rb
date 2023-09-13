922864a76b706255c93f32318776c61ab0868926
Jose Zepeda <jose-zromero@hotmail.com>
2023-09-12 19:57:16 -0600

    Add strong paramters to accep new values from form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   root to: "pages#home"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :quotes
   # Defines the root path route ("/")
